69dabe9120502df2f5f88ff47c336bc3447aec28
clementbriens <contact.clement.briens@gmail.com>
2022-02-02 00:01:09 +0100

    New rules and ES mapping

---
+++ utils/es_mapping.py
@@ -0,0 +1,10 @@
+mapping = ['id', 'text', 'in_reply_to_user_id', 'in_reply_to_status_id',
+'geo', 'coordinates', 'place', 'contributors', 'possibly_sensitive',
+'filter_level', 'lang', 'is_quote_status',
+'quote_count', 'reply_count', 'retweet_count', 'favorite_count', 'timestamp_ms']
+
+user_mapping = ['id', 'name', 'screen_name', 'location', 'url', 'description',
+'translator_type', 'protected', 'verified', 'followers_count', 'friends_count',
+'listed_count', 'favourites_counts', 'statuses_count', 'created_at', 'utc_offset',
+'time_zone', 'geo_enabled', 'lang', 'contributors_enabled', 'is_translator',
+'profile_use_background_image', 'default_profile', 'default_profile_image', 'witheld_in_countries']
